decb571c1b6891ec77d4763c26ea409cd5e5e18e
Tianyu Han <petertianyuhan@hotmail.com>
2022-05-19 12:46:17 +0200

    update eval and sampler

--- from_song/cs_mri.py
+++ from_song/cs_mri.py
@@ -5,9 +5,10 @@ import numpy as np
 import torch
 import torch.fft as fft
 
+from utils import batch_mul
 from models import utils as mutils
 from sampling import get_predictor, get_corrector, shared_predictor_update_fn, shared_corrector_update_fn
-from utils import batch_mul
+
 
 
 def get_cartesian_mask(shape, n_keep=30):
@@ -66,7 +67,7 @@ def kspace_to_image(kspace, axes):
   ) * shape
 
 
-def get_masks(config, img):
+def get_masks(config):
   if config.sampling.task == 'mri':
     mask = get_cartesian_mask((config.data.image_size, config.data.image_size), n_keep=config.sampling.n_projections)
     mask = mask[None, :, :, None].astype(np.float32)
@@ -90,14 +91,14 @@ def merge_known_with_mask(config, x_space, known, mask, coeff=1.):
     raise ValueError(f"task {config.sampling.mask} not recognized.")
 
 
-def get_cs_solver(config, sde, model, shape, inverse_scaler, eps=1e-5):
+def get_cs_solver(config, sde, shape, inverse_scaler, eps=1e-5):
   cs_solver = config.sampling.cs_solver
   # Probability flow ODE sampling with black-box ODE solvers
   predictor = get_predictor(config.sampling.predictor.lower())
   corrector = get_corrector(config.sampling.corrector.lower())
 
   if cs_solver.lower() == 'projection':
-    sampling_fn = get_projection_sampler(config, sde, model, shape, predictor, corrector,
+    sampling_fn = get_projection_sampler(config, sde, shape, predictor, corrector,
                                          inverse_scaler,
                                          n_steps=config.sampling.n_steps_each,
                                          probability_flow=config.sampling.probability_flow,
@@ -109,79 +110,78 @@ def get_cs_solver(config, sde, model, shape, inverse_scaler, eps=1e-5):
   return sampling_fn
 
 
-def get_projection_sampler(config, sde, model, shape, predictor, corrector,
+def get_projection_sampler(config, sde, shape, predictor, corrector,
                            inverse_scaler, n_steps=1,
                            probability_flow=False, continuous=True,
                            denoise=True, eps=1e-5, device='cuda'):
-    if config.sampling.task == 'mri':
-        to_space = lambda x: get_kspace(x, (1, 2))
-        from_space = lambda x: kspace_to_image(x, (1, 2)).real
-    else:
-        raise ValueError(f'Task {config.sampling.task} not recognized.')
+  if config.sampling.task == 'mri':
+      to_space = lambda x: get_kspace(x, (1, 2))
+      from_space = lambda x: kspace_to_image(x, (1, 2)).real
+  else:
+      raise ValueError(f'Task {config.sampling.task} not recognized.')
 
-    def get_inpaint_update_fn(update_fn):
-        def inpaint_update_fn(model, x, t, mask, known, coeff):
-            with torch.no_grad():
-                x_space = to_space(x)
-                mean, std = sde.marginal_prob(known, t)
-                noise = torch.randn_like(x)
-                noise_space = to_space(noise)
-                noisy_known = mean + batch_mul(std, noise_space)
-                
-                x_space = merge_known_with_mask(config, x_space, noisy_known, mask, coeff)
-                x = from_space(x_space)
-                
-                x, _ = update_fn(x, t, model=model)
-                return x
+  def get_inpaint_update_fn(update_fn):
+    def inpaint_update_fn(model, x, t, mask, known, coeff):
+      with torch.no_grad():
+          x_space = to_space(x)
+          mean, std = sde.marginal_prob(known, t)
+          noise = torch.randn_like(x)
+          noise_space = to_space(noise)
+          noisy_known = mean + batch_mul(std, noise_space)
+          
+          x_space = merge_known_with_mask(config, x_space, noisy_known, mask, coeff)
+          x = from_space(x_space)
+          
+          x, _ = update_fn(x, t, model=model)
+          return x
+    return inpaint_update_fn   
 
-        return inpaint_update_fn   
+  def projection_sampler(model, img, coeff, snr):
+    with torch.no_grad():
+      x = sde.prior_sampling(shape).to(device)
+      mask = get_masks(config).to(device)
+      known = get_known(config, img).to(device)
 
-    def projection_sampler(model, img, coeff, snr):
-        with torch.no_grad():
-            x = sde.prior_sampling(shape).to(device)
-            mask = get_masks(config, img).to(device)
-            known = get_known(config, img).to(device)
+      predictor_update_fn = functools.partial(shared_predictor_update_fn,
+                                              sde=sde,
+                                              predictor=predictor,
+                                              probability_flow=probability_flow,
+                                              continuous=continuous)
+      corrector_update_fn = functools.partial(shared_corrector_update_fn,
+                                              sde=sde,
+                                              corrector=corrector,
+                                              continuous=continuous,
+                                              snr=snr,
+                                              n_steps=n_steps)
 
-            predictor_update_fn = functools.partial(shared_predictor_update_fn,
-                                                    sde=sde,
-                                                    predictor=predictor,
-                                                    probability_flow=probability_flow,
-                                                    continuous=continuous)
-            corrector_update_fn = functools.partial(shared_corrector_update_fn,
-                                                    sde=sde,
-                                                    corrector=corrector,
-                                                    continuous=continuous,
-                                                    snr=snr,
-                                                    n_steps=n_steps)
+      cs_predictor_update_fn = get_inpaint_update_fn(predictor_update_fn)
+      cs_corrector_update_fn = get_inpaint_update_fn(corrector_update_fn)
 
-            cs_predictor_update_fn = get_inpaint_update_fn(predictor_update_fn)
-            cs_corrector_update_fn = get_inpaint_update_fn(corrector_update_fn)
+      timesteps = torch.linspace(sde.T, eps, sde.N, device=device)
 
-            timesteps = torch.linspace(sde.T, eps, sde.N, device=device)
+      def loop_body(x, i):
+        t = timesteps[i]
+        vec_t = torch.ones(shape[0], device=t.device) * t
+        x = cs_corrector_update_fn(model, x, vec_t, mask, known, coeff)
+        x = cs_predictor_update_fn(model, x, vec_t, mask, known, coeff)
+        output = x
+        return x, output
+      
+      all_samples = []
+      for i in range(sde.N):
+        x, all_sample = loop_body(x, i)
+        all_samples.append(all_sample)
+      output = all_samples[-1]    
 
-            def loop_body(x, i):
-                t = timesteps[i]
-                vec_t = torch.ones(shape[0], device=t.device) * t
-                x = cs_corrector_update_fn(model, x, vec_t, mask, known, coeff)
-                x = cs_predictor_update_fn(model, x, vec_t, mask, known, coeff)
-                output = x
-                return x, output
-            
-            all_samples = []
-            for i in range(sde.N):
-                x, all_sample = loop_body(x, i)
-                all_samples.append(all_sample)
-            output = all_samples[-1]    
+      if denoise:
+        t_eps = torch.full((output.shape[0],), eps)
+        k, std = sde.marginal_prob(torch.ones_like(output), t_eps) # somehow both args are not specifiy 'device'
+        score_fn = mutils.get_score_fn(sde, model, train=False, continuous=continuous, return_state=False)
+        score = score_fn(output, t_eps)
+        output = output / k + batch_mul(std ** 2, score / k)
+        output_space = to_space(output)
+        output_space = merge_known_with_mask(config, output_space, known, mask, 1.)
+        output = from_space(output_space)
+      return inverse_scaler(output)
 
-            if denoise:
-                t_eps = torch.full((output.shape[0],), eps)
-                k, std = sde.marginal_prob(torch.ones_like(output), t_eps) # somehow both args are not specifiy 'device'
-                score_fn = mutils.get_score_fn(sde, model, train=False, continuous=continuous, return_state=False)
-                score = score_fn(output, t_eps)
-                output = output / k + batch_mul(std ** 2, score / k)
-                output_space = to_space(output)
-                output_space = merge_known_with_mask(config, output_space, known, mask, 1.)
-                output = from_space(output_space)
-            return inverse_scaler(output)
-
-    return projection_sampler
+  return projection_sampler
--- from_song/eval.py
+++ from_song/eval.py
@@ -1,28 +1,25 @@
 import os
 import tqdm
+import imageio
+import logging
 import numpy as np
-from PIL import Image, ImageDraw
-from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+from absl import flags
 
-import tensorflow as tf
 import torch
-import torchvision.transforms as transforms
-import flax
-import jax
-import jax.numpy as jnp
+import tensorflow as tf
 
 import cs
 import losses
 import datasets
 import sde_lib
-import piq
 from models import utils as mutils
 from utils import restore_checkpoint
 from models.ema import ExponentialMovingAverage
 
+FLAGS = flags.FLAGS
 
-def evaluate(config, workdir, eval_folder="eval"):
-  """Evaluate trained models.
+def eval_sample(config, workdir, ckpt_name, eval_folder="eval", mar=False):
+  """Evaluate trained models by draw samples from it.
 
   Args:
     config: Configuration to use.
@@ -35,26 +32,19 @@ def evaluate(config, workdir, eval_folder="eval"):
   tf.io.gfile.makedirs(eval_dir)
 
   # Build data pipeline
-  test_data_dir = {
-    'ct2d_320': 'LIDC_320.npz',
-    'ldct_512': 'LDCT.npz',
-    'brats': 'BraTS.npz'
-  }[config.data.dataset]
-  test_data_dir = os.path.join('test_data', test_data_dir)
-  test_imgs = np.load(test_data_dir)['all_imgs']
-  test_imgs = test_imgs.reshape((jax.process_count(), -1, *test_imgs.shape[1:]))[jax.process_index()]
-  mar = False
+  train_ds, eval_ds, _ = datasets.get_dataset(config,
+                                            uniform_dequantization=config.data.uniform_dequantization,
+                                            evaluation=True)
 
   # Create data normalizer and its inverse
-  scaler = datasets.get_data_scaler(config)
-  inverse_scaler = datasets.get_data_inverse_scaler(config)
+  scaler = datasets.get_data_scaler(config)                   # Rescale data from [0, 1] to [-1, 1]
+  inverse_scaler = datasets.get_data_inverse_scaler(config)   # Scale back from [-1, 1] to [0, 1]
 
   # Initialize model
   score_model = mutils.create_model(config)
   optimizer = losses.get_optimizer(config, score_model.parameters())
   ema = ExponentialMovingAverage(score_model.parameters(), decay=config.model.ema_rate)
   state = dict(optimizer=optimizer, model=score_model, ema=ema, step=0)
-
   checkpoint_dir = os.path.join(workdir, "checkpoints")
 
   # Setup SDEs
@@ -74,14 +64,14 @@ def evaluate(config, workdir, eval_folder="eval"):
   sampling_shape = (config.eval.batch_size,
                     config.data.image_size, config.data.image_size,
                     config.data.num_channels)
-  cs_solver = cs.get_cs_solver(config, sde, score_model, sampling_shape, inverse_scaler, eps=sampling_eps)
+  cs_solver = cs.get_cs_solver(config, sde, sampling_shape, inverse_scaler, eps=sampling_eps)
   
   # load checkpoint to 'score_model'
-  ckpt = 15
-  ckpt_path = os.path.join(checkpoint_dir, f'checkpoint_{ckpt}.pth')
+  ckpt_path = os.path.join(checkpoint_dir, ckpt_name)
   state = restore_checkpoint(ckpt_path, state, device=config.device)
   ema.copy_to(score_model.parameters())
 
+  # prepare sampling hyperparameters 
   hyper_params = {
     'projection': [config.sampling.coeff, config.sampling.snr],
     'langevin_projection': [config.sampling.coeff, config.sampling.snr],
@@ -89,39 +79,31 @@ def evaluate(config, workdir, eval_folder="eval"):
     'baseline': [config.sampling.projection_sigma_rate, config.sampling.snr]
   }[config.sampling.cs_solver]
 
-  per_host_batch_size = config.eval.batch_size
-  num_batches = int(np.ceil(len(test_imgs) / per_host_batch_size))
-
-  # Create a circular mask
-  img_size = config.data.image_size
-  mask = Image.new('L', (img_size, img_size), 0)
-  draw = ImageDraw.Draw(mask)
-  draw.pieslice([0, 0, img_size, img_size], 0, 360, fill=255)
-  toTensor = transforms.ToTensor()
-  mask = toTensor(mask)[0]
-
   all_samples = []
-  for batch in tqdm.tqdm(range(num_batches)):
+  imgs = []
+  undersamples = []
+  masks = []
+  
+  eval_iter = iter(eval_ds) 
+  for r, batch in tqdm.tqdm(enumerate(eval_iter)):
+    logging.info("sampling -- ckpt: %s, round: %d" % (ckpt_name, r))
     if not mar:
-      current_batch = jnp.asarray(test_imgs[batch * per_host_batch_size:
-                                            min((batch + 1) * per_host_batch_size,
-                                                len(test_imgs))], dtype=jnp.float32) / 255.
+      current_batch = torch.from_numpy(batch['image']._numpy()).to(config.device).float()
+      current_batch = current_batch.permute(0, 3, 1, 2)
+      current_batch = current_batch.reshape((-1, *sampling_shape))
+      img = scaler(current_batch)
     else:
       raise NotImplementedError("task unknown.")
 
-    n_effective_samples = len(current_batch)
-    if n_effective_samples < per_host_batch_size:
-      pad_len = per_host_batch_size - len(current_batch)
-      current_batch = jnp.pad(current_batch, ((0, pad_len), (0, 0), (0, 0)),
-                              mode='constant', constant_values=0.)
-
-    current_batch = current_batch.reshape((-1, *sampling_shape))
-    img = scaler(current_batch)
-
-    rng, *step_rng = jax.random.split(rng, jax.local_device_count() + 1)
-    step_rng = jnp.asarray(step_rng)
-    samples = cs_solver(step_rng, pstate, img, *hyper_params)
-
+    samples = cs_solver(score_model, img, *hyper_params) # masks generated inside the cs_solver
     samples = np.clip(np.asarray(samples), 0., 1.)
-    samples = samples.reshape((-1, config.data.image_size, config.data.image_size, 1))[:n_effective_samples]
+    samples = samples.reshape((-1, config.data.image_size, config.data.image_size, 1))
+    
     all_samples.extend(samples)
+    imgs.extend(img)
+  
+  for sample in all_samples:
+    # save original, undersampled, reconstructed imgs, and mask
+    # also, save original and reconstructed into seperate folder for PSNR and SSIM computation
+    sample  = np.clip(np.rint(sample * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
+    imageio.imwrite(os.path.join())
